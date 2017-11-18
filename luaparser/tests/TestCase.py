dd26dfaa6697604412ac3a8e71ced5c31e4c0f8a
boolangery <eliott.dumeix@gmail.com>
2017-11-18 21:10:30 +0100

    tests: fix TestCase

--- luaparser/tests/TestCase.py
+++ luaparser/tests/TestCase.py
@@ -5,6 +5,7 @@ class TestCase(unittest.TestCase):
     def assertAstEqual(self, ast, other):
         if isinstance(ast, Node) and isinstance(other, Node):
             self.assertEqual(len(ast.childs), len(other.childs))
+            self.assertIsInstance(ast, other.__class__)
             for i, child in enumerate(ast.childs):
                 self.assertAstEqual(child, other.childs[i])
         else:
