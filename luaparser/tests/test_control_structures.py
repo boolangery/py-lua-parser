0c21c2aff45dd571669025443bcea171c2e29680
boolangery <eliott.dumeix@gmail.com>
2017-11-23 22:07:55 +0100

    tests: if, elseif, else.

--- luaparser/tests/test_control_structures.py
+++ luaparser/tests/test_control_structures.py
@@ -4,11 +4,14 @@ from luaparser.astNodes import *
 import textwrap
 
 
+''' ----------------------------------------------------------------------- '''
+''' 3.3.4 – Control Structures                                              '''
+''' ----------------------------------------------------------------------- '''
 class ControlStructureTestCase(tests.TestCase):
     def setUp(self):
         self.parser = Parser()
 
-    def test_set_number(self):
+    def test_while(self):
         ast = self.parser.srcToAST(textwrap.dedent("""
             while a[i] do
               i = i + 1
@@ -22,5 +25,75 @@ class ControlStructureTestCase(tests.TestCase):
                 ]))
             ])
         ))
+        self.assertAstEqual(exp, ast)
 
+    def test_repeat_until(self):
+        ast = self.parser.srcToAST(textwrap.dedent("""
+            repeat        
+            until true
+            """))
+        exp = Chunk(Block(RepeatStat([Block(None), TrueExpr()])))
+        self.assertAstEqual(exp, ast)
+
+    def test_if(self):
+        ast = self.parser.srcToAST(textwrap.dedent("""
+            if true then    
+            end
+            """))
+        exp = Chunk(Block(IfStat([TrueExpr(), Block(None)])))
+        self.assertAstEqual(exp, ast)
+
+    def test_if_exp(self):
+        ast = self.parser.srcToAST(textwrap.dedent("""
+            if (a<2) then    
+            end
+            """))
+        exp = Chunk(Block(IfStat([LessThanOpExpr([IdExpr('a'), NumberExpr(2)]), Block(None)])))
+        self.assertAstEqual(exp, ast)
+
+    def test_if_elseif(self):
+        ast = self.parser.srcToAST(textwrap.dedent("""
+            if true then 
+            elseif false then     
+            end
+            """))
+        exp = Chunk(Block(
+            IfStat([
+                TrueExpr(), Block(None), ElseIfStat([FalseExpr(), Block(None)])
+            ])
+        ))
+        self.assertAstEqual(exp, ast)
+
+    def test_if_elseif_else(self):
+        ast = self.parser.srcToAST(textwrap.dedent("""
+            if true then 
+            elseif false then  
+            else   
+            end
+            """))
+        exp = Chunk(Block(
+            IfStat([
+                TrueExpr(), Block(None),
+                ElseIfStat([FalseExpr(), Block(None)]),
+                ElseStat(Block(None))
+            ])
+        ))
+        self.assertAstEqual(exp, ast)
+
+    def test_if_elseif_elseif_else(self):
+        ast = self.parser.srcToAST(textwrap.dedent("""
+            if true then 
+            elseif false then  
+            elseif 42 then 
+            else   
+            end
+            """))
+        exp = Chunk(Block(
+            IfStat([
+                TrueExpr(), Block(None),
+                ElseIfStat([FalseExpr(), Block(None)]),
+                ElseIfStat([NumberExpr(42), Block(None)]),
+                ElseStat(Block(None))
+            ])
+        ))
         self.assertAstEqual(exp, ast)
