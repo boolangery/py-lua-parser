e6f9702259962729eb00b61a710ab22db649da64
boolangery <eliott.dumeix@gmail.com>
2017-12-09 21:46:48 +0100

    move test_asignment to test_statements

--- luaparser/tests/test_statements.py
+++ luaparser/tests/test_statements.py
@@ -22,3 +22,41 @@ class StatementsTestCase(tests.TestCase):
             LocalAssignStat(targets=[NameExpr('b')],values=[])
         ]))
         self.assertEqual(exp, ast)
+
+        """
+    3.3.3 – Assignment
+    """
+    def test_set_number(self):
+        ast = self.parser.srcToAST("i=3")
+        exp = Chunk(body=Block(body=[
+            AssignStat(targets=[NameExpr('i')],values=[NumberExpr(3)])
+        ]))
+        self.assertEqual(exp, ast)
+
+    def test_set_string(self):
+        ast = self.parser.srcToAST('i="foo bar"')
+        exp = Chunk(body=Block(body=[
+            AssignStat(targets=[NameExpr('i')],values=[StringExpr('foo bar')])
+        ]))
+        self.assertEqual(exp, ast)
+
+    def test_set_array_index(self):
+        ast = self.parser.srcToAST('a[i] = 42')
+        exp = Chunk(body=Block(body=[
+            AssignStat(targets=[IndexExpr(idx=NameExpr('i'), value=NameExpr('a'))], values=[NumberExpr(42)])
+        ]))
+        self.assertEqual(exp, ast)
+
+    def test_set_table_index(self):
+        ast = self.parser.srcToAST('_ENV.x = val')
+        exp = Chunk(body=Block(body=[
+            AssignStat(targets=[IndexExpr(idx=NameExpr('x'), value=NameExpr('_ENV'))], values=[NameExpr('val')])
+        ]))
+        self.assertEqual(exp, ast)
+
+    def test_set_multi(self):
+        ast = self.parser.srcToAST('x, y = y, x')
+        exp = Chunk(body=Block(body=[
+            AssignStat(targets=[NameExpr('x'), NameExpr('y')],values=[NameExpr('y'), NameExpr('x')])
+        ]))
+        self.assertEqual(exp, ast)
