70497fcacca56d537eb6852d152dd89b670b1e36
boolangery <eliott.dumeix@gmail.com>
2018-01-02 21:05:45 +0100

    pprint: fix token printer.

--- luaparser/pprint/TokenPrinter.py
+++ luaparser/pprint/TokenPrinter.py
@@ -8,17 +8,20 @@ class TokenPrinter():
         for token in tokens:
             # extend current source:
             if token.line > len(self._lines):
-                self._lines.extend(repeat('', (token.line - len(self._lines) + 1)))
+                self._lines.extend(repeat('', (token.line - len(self._lines))))
 
             # Insert in source line:
+            line = token.line -1
+            text = token.text
             position = token.column
-            s = self._lines[token.line]
-
+            s = self._lines[line]
             # extend string
             if position > len(s):
                 s += ' ' * (position - len(s))
 
             # Use slicing to extract portion to replace:
-            s = s[:position] + token.text + s[position+len(token.text):]
-            self._lines[token.line] = s
+            s = s[:position] + text + s[position+len(text):]
+            self._lines[line] = s
+
+
         return '\n'.join(self._lines)
