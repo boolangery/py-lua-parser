579b15de67d77f30daa790ae7ef43e1f4dfd2d5f
boolangery <eliott.dumeix@gmail.com>
2018-01-11 22:23:19 +0100

    tests: visitor and walker.

---
+++ luaparser/tests/test_ast.py
@@ -0,0 +1,39 @@
+from luaparser.utils  import tests
+from luaparser import ast
+from luaparser.astnodes import *
+import textwrap
+
+class TokensTestCase(tests.TestCase):
+    def test_walk_1(self):
+        src = textwrap.dedent("""
+            local a = 1
+            """)
+        tree = ast.parse(src)
+        chunk, block, local, name, number = False, False, False, False, False
+        for node in ast.walk(tree):
+            if isinstance(node, Chunk): chunk = True
+            if isinstance(node, Block): block = True
+            if isinstance(node, LocalAssign): local = True
+            if isinstance(node, Name): name = True
+            if isinstance(node, Number): number = True
+        self.assertTrue(chunk)
+        self.assertTrue(block)
+        self.assertTrue(local)
+        self.assertTrue(name)
+        self.assertTrue(number)
+
+    def test_visitor_1(self):
+        src = textwrap.dedent("""
+            local a = 1
+            """)
+
+        called = False
+        class NumberVisitor(ast.ASTVisitor):
+            def visit_Number(self, node):
+                nonlocal called
+                called = True
+
+        tree = ast.parse(src)
+        NumberVisitor().visit(tree)
+        self.assertTrue(called)
+
