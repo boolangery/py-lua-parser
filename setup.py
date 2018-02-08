939a9bcf365ad67020b617f02d8fb8655a8b0fce
boolangery <eliott.dumeix@gmail.com>
2018-02-08 20:46:51 +0100

    1.1.2 version
    * fix lua number, leading zero is allowed

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup
 
-__version__ = '1.1'
+__version__ = '1.1.2'
 
 setup(
-  name = 'luaparser',
-  version = __version__,
-  description = 'A lua parser in Python',
-  url = 'https://github.com/boolangery/py-lua-parser',
-  download_url = 'https://github.com/boolangery/py-lua-parser/archive/' + __version__ + '.tar.gz',
-  author = 'Eliott Dumeix',
-  author_email = 'eliott.dumeix@gmail.com',
-  license = 'MIT',
-  packages = ['luaparser', 'luaparser.parser', 'luaparser.utils', 'luaparser.tests'],
-  zip_safe = False,
-  classifiers = [
+    name = 'luaparser',
+    version = __version__,
+    description = 'A lua parser in Python',
+    url = 'https://github.com/boolangery/py-lua-parser',
+    download_url = 'https://github.com/boolangery/py-lua-parser/archive/' + __version__ + '.tar.gz',
+    author = 'Eliott Dumeix',
+    author_email = 'eliott.dumeix@gmail.com',
+    license = 'MIT',
+    packages = ['luaparser', 'luaparser.parser', 'luaparser.utils', 'luaparser.tests'],
+    zip_safe = False,
+    classifiers = [
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
-  ],
-  install_requires = ['antlr4-python3-runtime<=4.7', 'llist<=0.4']
+    ],
+    install_requires = ['antlr4-python3-runtime<=4.7', 'llist<=0.4']
 )
